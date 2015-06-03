2501476fad2b61185cd2bc2ffe3ab3a94c97ce88
andela-tsolarin <oluwatoni.sodara@andela.co>
2015-06-03 01:28:56 +0100

    Modified email client to use nodemailer

--- app/controllers/emails.server.controller.js
+++ app/controllers/emails.server.controller.js
@@ -1,48 +1,41 @@
 var smtpConnection = require('smtp-connection');
+var nodemailer = require('nodemailer'),
+    smtpTransport = require('nodemailer-smtp-transport');
 
-var emailClient = new smtpConnection({
-  host: 'smtp.gmail.com',
-  port: 25
-});
-
-emailClient.connect(function(){
-  console.log('Connected...');
-});
+var transporter;
 
 // Authenticate user
 exports.login = function(req, res) {
-  emailClient.login({
-    user: req.body.username,
-    pass: req.body.password
-  }, function(err) {
-    if (err) {
-      console.log(err);
-      res.json({
-        "message": "Login failed"
-      });
-    } else {
-      res.json({
-        "message": "Login successful"
-      });
+  transporter = nodemailer.createTransport(smtpTransport({
+    host: 'smtp.gmail.com',
+    port: 25,
+    auth: {
+      user: req.body.username,
+      pass: req.body.password
     }
+  }));
+  res.json({
+    "message": "Logged in successfully"
   });
 }
 
 
 // Send email
 exports.send = function(req, res) {
-  emailClient.send({
+  transporter.sendMail({
     from: req.body.from,
-    to: req.body.to
-  }, req.body.message, function(err, info){
+    to: req.body.to,
+    subject: req.body.subject,
+    html: req.body.message
+  }, function(error, response){
 
-    if(err){
-      console.log(err);
+    if(error){
+      console.log(error);
       res.json({
         "message": "Message sending failed"
       });
     } else {
-      console.log(info);
+      console.log(response);
       res.json({
         "message": "Message sent successfully"
       });
@@ -53,7 +46,7 @@ exports.send = function(req, res) {
 
 // Log user out
 exports.logout = function(req, res) {
-  emailClient.quit();
+  transporter.close();
   res.json({
     "message": "User logged out successfully"
   });
