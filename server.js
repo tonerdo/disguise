02e9818193456d2666f789cc36b758e69344e182
andela-tsolarin <oluwatoni.sodara@andela.co>
2015-06-04 14:54:26 +0100

    Moved dotenv requirement to server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require('dotenv').load();
 var mongoose = require('mongoose'),
     config = require('./config/config')();
 
 var db = mongoose.connect(config.db.uri);
+mongoose.connection.once('open', function(){
+  console.log("Connection successful");
+});
 
 var appServer = require('./config/express')();
 var smtpServer = require('./config/smtp-server')();
