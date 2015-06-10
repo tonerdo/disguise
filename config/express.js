6b12beac43af2e951a0775eaf07848c1cd2211bf
andela-tsolarin <oluwatoni.sodara@andela.co>
2015-06-10 17:40:35 +0100

    Use process.cwd() to serve static files

--- config/express.js
+++ config/express.js
@@ -14,7 +14,7 @@ module.exports = function() {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
-  app.use(express.static('public'));
+  app.use(express.static(process.cwd() + '/public'));
 
   app.use(session(
     {
