92bf1d488e48bd2703c7991584d9a8a38b5a8073
andela-tsolarin <oluwatoni.sodara@andela.co>
2015-06-10 20:12:32 +0100

    Tweaked home page UI

--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -8,6 +8,10 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
         templateUrl: './views/home.client.view.html',
         controller: 'HomeCtrl'
     })
+    .state('mail', {
+        url:'/mail',
+        templateUrl: './views/mail.client.view.html'
+    })
     .state('inbox', {
         url:'/mail/inbox',
         templateUrl: './views/inbox.client.view.html'
