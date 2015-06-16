92334cdf8d27187839e97ed34d96afe2f0db7446
andela-tsolarin <oluwatoni.sodara@andela.co>
2015-06-16 20:36:38 +0100

    Use file string instead of stream

--- config/mailserver.js
+++ config/mailserver.js
@@ -10,30 +10,41 @@ var MailParser = require("mailparser").MailParser,
     mailparser = new MailParser();
 
 
-// Mail parser event handlers
-mailparser.on('end', function(message){
-
-  var recipient = message.to[0].address;
-  var username = recipient.split('@')[0];
-  username = username.toLowerCase();
-  message.read = false;
-
-  var email = JSON.stringify(message);
-
-  // Insert message details into database
-  User.findOneAndUpdate(
-    username,
-    {$push: {"received": email}},
-    {safe: true, upsert: true},
-    function(err, model) {
-      if(err) console.log('Received messages append for mailbox ' + recipient + '. Error: ' + err);
-      else console.log('Message recieved to mailbox: ' + message.to[0].address);
-    }
-  );
-
-});
-
 module.exports = function() {
+
+
+  // Mail parser event handlers
+  mailparser.on('end', function(message){
+
+    var recipient = message.to[0].address;
+    var username = recipient.split('@')[0];
+    username = username.toLowerCase();
+    message.read = false;
+
+    console.log(message);
+    var email = JSON.stringify(message);
+
+    // Insert message details into database
+    User.findOneAndUpdate(
+      username,
+      {$push: {"received": email}},
+      {safe: true, upsert: true},
+      function(err, model) {
+
+        if(model) console.log('Model is set')
+        else console.log('Model is null');
+
+        if(err) console.log('Received messages append for mailbox ' + recipient + '. Error: ' + err);
+        else console.log('Message recieved to mailbox: ' + message.to[0].address);
+      }
+    );
+
+  });
+
+  var parse = function(message) {
+    mailparser.write(message);
+    mailparser.end();
+  };
 
   var mail = new smtp({
 
@@ -79,8 +90,21 @@ module.exports = function() {
         // Write to file
         var filename = random.uuid4() + '.eml';
         fs.writeFile(filename, content, function(err){
-          if (err) console.log("Error writing to file: " + err);
-          else fs.createReadStream(filename).pipe(mailparser);
+
+          if (err) {
+           console.log("Error writing to file: " + err)
+          } else {
+
+            fs.readFile(filename, function(err, data){
+              
+              if(!err) {
+                parse(data);
+              }
+
+            });
+
+          }
+
         });
         
         // Parse email message
