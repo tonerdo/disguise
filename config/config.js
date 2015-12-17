1d84c586162b21ad443b7d445296fd5f4b29af65
Toni <toni.edward@outlook.com>
2015-12-17 21:04:50 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,15 +1,15 @@
 module.exports = function() {
 
   var server = {
-    "port": process.env.PORT
+    "port": process.env.PORT || 8080
   };
 
   var mail = {
-    "port": process.env.MAIL_PORT
+    "port": process.env.MAIL_PORT || 1025
   };
 
   var db = {
-    "uri": process.env.MONGODB
+    "uri": process.env.MONGODB_DEV || "mongodb://" + process.env.MONGODB_PORT_27017_TCP_ADDR + ":" + process.env.MONGODB_PORT_27017_TCP_PORT + "/disguise"
   };
 
   var jwt = {
