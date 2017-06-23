0644ea6c35ef701b7e3d0d1c2af7f2ddf14ccebf
Oluwatoni Solarin-Sodara <oluwatoni.sodara@ithaka.org>
2017-06-23 12:24:49 +0100

    Log mail server listening status

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ var mongoose = require('mongoose'),
 
 var db = mongoose.connect(config.db.uri);
 mongoose.connection.once('open', function(){
-  console.log("Database Connection successful");
+  console.log("Database connection successful");
 });
 
 var appServer = require('./config/express')();
 var mailServer = require('./config/mailserver')();
 
 appServer.listen(config.server.port, function(){
-  console.log("Server listening on port " + config.server.port);
+  console.log("Web server listening on port " + config.server.port);
 });
-mailServer.listen(config.mail.port);
+
+mailServer.listen(config.mail.port, function(){
+  console.log("Mail server listening on port " + config.mail.port);
+});
 
 exports = module.exports = appServer;
 exports = module.exports = mailServer;
